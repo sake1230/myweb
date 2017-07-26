a7ea0107f275265d28b9923931583b0bb3827da4
success <744827115@qq.com>
2017-07-26 16:22:33 +0800

    第二次提交

---
+++ web002/src/main/java/com/bwie/dao/DepartmentMapper.java
@@ -0,0 +1,30 @@
+package com.bwie.dao;
+
+import com.bwie.bean.Department;
+import com.bwie.bean.DepartmentExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface DepartmentMapper {
+    long countByExample(DepartmentExample example);
+
+    int deleteByExample(DepartmentExample example);
+
+    int deleteByPrimaryKey(Integer did);
+
+    int insert(Department record);
+
+    int insertSelective(Department record);
+
+    List<Department> selectByExample(DepartmentExample example);
+
+    Department selectByPrimaryKey(Integer did);
+
+    int updateByExampleSelective(@Param("record") Department record, @Param("example") DepartmentExample example);
+
+    int updateByExample(@Param("record") Department record, @Param("example") DepartmentExample example);
+
+    int updateByPrimaryKeySelective(Department record);
+
+    int updateByPrimaryKey(Department record);
+}
